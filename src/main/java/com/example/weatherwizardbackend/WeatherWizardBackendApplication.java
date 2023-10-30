2cd3a27398d25e091bbc152df9c04163ca900707
Caleb Liu <liucaleb8@gmail.com>
2023-10-30 13:42:56 -0400

    Set up backend

---
+++ src/main/java/com/example/weatherwizardbackend/WeatherWizardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherwizardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWizardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWizardBackendApplication.class, args);
+	}
+
+}
