bc8ed44de167c9f5650ff9d9d83473fe9f2a8f19
Jess <j.alfahad@gmail.com>
2023-11-08 15:49:38 -0500

    begin adding database saving code in main.py and add new .py file to handle db

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask, request, render_template
+import base64
+
+from flask import Flask, request, render_template, csv, numpy, flash
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
 import os
 
+from create_db import FileContent
+
 appFlask = Flask(__name__)
+
+db = SQLAlchemy(appFlask)
 
 @appFlask.route('/', methods=['GET', 'POST'])
 def index():
@@ -26,6 +33,17 @@ def index():
                 file_path = os.path.join(folder_path, filename)
                 file.save(file_path)
                 image_files.append(file_path.replace('\\', '/'))
+                # db code
+                # data = file.read()
+                # render_file = render_picture(data)
+                # text = request.form['text']
+                # location = request.form['location']
+                #
+                # newFile = FileContent(name=file.filename, data=data,
+                #                       rendered_data=render_file, text=text, location=location)
+                # db.session.add(newFile)
+                # db.session.commit()
+                # flash(f'Pic {newFile.name} uploaded Text: {newFile.text} Location:{newFile.location}')
 
         # Debug: Print the contents of image_files
         print(image_files)
@@ -35,7 +53,16 @@ def index():
         else:
             return render_template('result.html', image_files=None)
 
+    # reader = csv.reader(open("test.csv", "rb"), delimiter=",")
+    # x = list(reader)
+    # result = numpy.array(x).astype("float")
+
     return render_template('index.html')
+
+def render_picture(data):
+
+    render_pic = base64.b64encode(data).decode('ascii')
+    return render_pic
 
 def allowed_file(filename):
     return '.' in filename and \
